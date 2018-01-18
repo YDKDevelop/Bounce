85330dfb5fdddff84599d2fd1bdd6aa89da28332
Developer <ydkzheng22@gmail.com>
2018-01-17 22:00:56 -0500

    Add files via upload

---
+++ desktop/src/com/mygdx/gameV2/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mygdx.gameV2.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mygdx.gameV2.B_Ball;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new B_Ball(), config);
+	}
+}
---
+++ html/src/com/mygdx/gameV2/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.mygdx.gameV2.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.mygdx.gameV2.B_Ball;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new B_Ball();
+        }
+}
